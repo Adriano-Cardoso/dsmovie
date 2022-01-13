5f2f67d98c4e44ab44046869f573dd323e365e02
PAMPACOMPASSO\adriano.cardoso <adriano.cardoso@compasso.com.br>
2022-01-12 21:48:45 -0300

    Security config

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
                     <a href="https://github.com/Adriano-Cardoso" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
+                            <p className="dsmovie-contact-link">/Adriano-Cardoso</p>
                         </div>
                     </a>
                 </div>
